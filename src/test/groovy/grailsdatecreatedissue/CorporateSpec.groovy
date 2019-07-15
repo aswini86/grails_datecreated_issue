3aa8c10350439bd96ab677bb4bd4152c25ffa0e1
aswinigbc <aswini@geekbull.in>
2019-07-16 02:06:29 +0530

    Added code for NPE.

---
+++ src/test/groovy/grailsdatecreatedissue/CorporateSpec.groovy
@@ -0,0 +1,18 @@
+package grailsdatecreatedissue
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class CorporateSpec extends Specification implements DomainUnitTest<Corporate> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ src/test/groovy/grailsdatecreatedissue/UpdateCacheServiceSpec.groovy
@@ -0,0 +1,18 @@
+package grailsdatecreatedissue
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class UpdateCacheServiceSpec extends Specification implements ServiceUnitTest<UpdateCacheService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
